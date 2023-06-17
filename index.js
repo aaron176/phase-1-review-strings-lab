f1ce826542812a191b54d1e18ea142e8e4e52cab
haroon <haroon.seena2017@gmail.com>
2023-06-17 18:32:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+const currentUser ="haroon"
+const welcomeMessage = `Welcome to Recoded, + ${currentUser}`
+const exitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`Welcome ${exitedWelcomeMessage.slice(0,1)}`
